8f9b5f1b25c6fd31102bb9b50248e80aa6eb37a1
Limon Farazi <abidislamabir@gmail.com>
2022-02-13 18:33:47 +0600

    some change

--- js/banking.js
+++ js/banking.js
@@ -2,7 +2,7 @@ document.getElementById('deposit-btn').addEventListener('click', function() {
     // get the amount deoposited
     let depositInput = document.getElementById('deposit-input');
     let depositAmount = depositInput.value;
-    console.log(depositAmount);
+    // console.log(depositAmount);
     // next
     const depositTotal = document.getElementById('deposit-total');
     depositTotal.innerText = depositAmount;
